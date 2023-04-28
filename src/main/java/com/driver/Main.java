7bda37e195db7576d2e102f846565d2d03bfca09
kamlesh-cyber <kamleshgupta2804@gmail.com>
2023-04-28 18:08:08 +0530

    Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+        //obj.name = "Kamlesh"; // private variable can't be accessed outside the class
+         obj.setName("Kamlesh");
+         String name = obj.getName();
+        System.out.println(name);
+    }
   
 }
